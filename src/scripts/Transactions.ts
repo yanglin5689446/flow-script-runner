75b07b174577568578a0875a239507c300669231
Scott <scott.ph.chou@gmail.com>
2022-03-01 14:27:47 +0800

    Update transfer flow token script

--- src/scripts/Transactions.ts
+++ src/scripts/Transactions.ts
@@ -155,17 +155,31 @@ import FungibleToken from ${getAddress("FungibleToken")}
 import FlowToken from ${getAddress("FlowToken")}
 
 transaction(amount: UFix64, to: Address) {
+
+    // The Vault resource that holds the tokens that are being transferred
     let sentVault: @FungibleToken.Vault
+
     prepare(signer: AuthAccount) {
-        let vaultRef = signer.borrow<&FlowToken.Vault>(from: /public/flowTokenVault)
+
+        // Get a reference to the signer's stored vault
+        let vaultRef = signer.borrow<&FlowToken.Vault>(from: /storage/flowTokenVault)
             ?? panic("Could not borrow reference to the owner's Vault!")
+
+        // Withdraw tokens from the signer's stored vault
         self.sentVault <- vaultRef.withdraw(amount: amount)
     }
 
     execute {
+
+        // Get the recipient's public account object
         let recipient = getAccount(to)
-        let receiverRef = recipient.getCapability(/public/flowTokenReceiver)!.borrow<&{FungibleToken.Receiver}>()
+
+        // Get a reference to the recipient's Receiver
+        let receiverRef = recipient.getCapability(/public/flowTokenReceiver)
+            .borrow<&{FungibleToken.Receiver}>()
             ?? panic("Could not borrow receiver reference to the recipient's Vault")
+
+        // Deposit the withdrawn tokens in the recipient's receiver
         receiverRef.deposit(from: <-self.sentVault)
     }
 }
