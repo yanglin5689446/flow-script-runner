30a7a4604b3ccd5f1b5ff71b2b74e2d17653cc3a
moojing <starts789@gmail.com>
2023-05-11 22:47:33 +0800

    feat: update goerli contract.

--- src/contracts/goerli.ts
+++ src/contracts/goerli.ts
@@ -1,54 +1,116 @@
 import { AbiItem } from "web3-utils";
 
-const contractAddress = "0xFB9688306D687F16C6d658Fa2A04e0fB59071212";
+const contractAddress = "0x009c403BdFaE357d82AAef2262a163287c30B739";
 const contractAbi: AbiItem[] = [
+  { inputs: [], stateMutability: "nonpayable", type: "constructor" },
   {
-    constant: true,
-    inputs: [],
-    name: "value",
-    outputs: [{ name: "", type: "uint256" }],
-    payable: false,
-    stateMutability: "view",
-    type: "function",
-  },
-  {
-    constant: false,
-    inputs: [{ name: "newValue", type: "uint256" }],
-    name: "setValue",
-    outputs: [],
-    payable: false,
-    stateMutability: "nonpayable",
-    type: "function",
-  },
-  {
-    constant: true,
-    inputs: [],
-    name: "value2",
-    outputs: [{ name: "", type: "uint256" }],
-    payable: false,
-    stateMutability: "view",
-    type: "function",
-  },
-  {
-    constant: false,
-    inputs: [{ name: "newValue", type: "uint256" }],
-    name: "setValue2",
-    outputs: [],
-    payable: false,
-    stateMutability: "nonpayable",
-    type: "function",
+    anonymous: false,
+    inputs: [
+      {
+        indexed: false,
+        internalType: "string",
+        name: "message",
+        type: "string",
+      },
+    ],
+    name: "donateMessage",
+    type: "event",
   },
   {
     anonymous: false,
-    inputs: [{ indexed: false, name: "value", type: "uint256" }],
+    inputs: [
+      {
+        indexed: false,
+        internalType: "uint256",
+        name: "value",
+        type: "uint256",
+      },
+    ],
     name: "updateValue",
     type: "event",
   },
   {
     anonymous: false,
-    inputs: [{ indexed: false, name: "value", type: "uint256" }],
+    inputs: [
+      {
+        indexed: false,
+        internalType: "uint256",
+        name: "value",
+        type: "uint256",
+      },
+    ],
     name: "updateValue2",
     type: "event",
+  },
+  {
+    inputs: [],
+    name: "_owner",
+    outputs: [{ internalType: "address", name: "", type: "address" }],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [{ internalType: "uint256", name: "a", type: "uint256" }],
+    name: "arithmeticError",
+    outputs: [{ internalType: "uint256", name: "", type: "uint256" }],
+    stateMutability: "pure",
+    type: "function",
+  },
+  {
+    inputs: [],
+    name: "customError",
+    outputs: [],
+    stateMutability: "pure",
+    type: "function",
+  },
+  {
+    inputs: [{ internalType: "string", name: "message", type: "string" }],
+    name: "donate",
+    outputs: [],
+    stateMutability: "payable",
+    type: "function",
+  },
+  {
+    inputs: [{ internalType: "uint256", name: "newValue", type: "uint256" }],
+    name: "setValue",
+    outputs: [],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+  {
+    inputs: [{ internalType: "uint256", name: "newValue", type: "uint256" }],
+    name: "setValue2",
+    outputs: [],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+  {
+    inputs: [{ internalType: "address", name: "newOwner", type: "address" }],
+    name: "transferOwnership",
+    outputs: [],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+  {
+    inputs: [],
+    name: "value",
+    outputs: [{ internalType: "uint256", name: "", type: "uint256" }],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [],
+    name: "value2",
+    outputs: [{ internalType: "uint256", name: "", type: "uint256" }],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [],
+    name: "withdraw",
+    outputs: [],
+    stateMutability: "nonpayable",
+    type: "function",
   },
 ];
 
