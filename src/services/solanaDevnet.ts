c00fbe4a916b217dd4e53d72a85412e4fc26aa70
Scott <scott.ph.chou@gmail.com>
2023-06-06 17:02:22 +0800

    fix: update solana mainnet rpc url

--- src/services/solanaDevnet.ts
+++ src/services/solanaDevnet.ts
@@ -14,7 +14,7 @@ const isMainnet = process.env.REACT_APP_NETWORK === "mainnet";
 const bloctoSDK = new BloctoSDK({
   solana: {
     net: isMainnet ? "mainnet-beta" : "devnet",
-    rpc: isMainnet ? "https://api.mainnet-beta.solana.com" : undefined,
+    rpc: isMainnet ? "https://api.metaplex.solana.com/" : undefined,
   },
   appId: process.env.REACT_APP_DAPP_ID,
 }) as ExtendedSolaneBloctoSDK;
