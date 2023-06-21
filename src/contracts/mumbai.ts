0372800aa1341538342b07394b83c4967875d1da
moojing <starts789@gmail.com>
2023-06-21 14:46:10 +0800

    feat: update evm dev contract address.

--- src/contracts/mumbai.ts
+++ src/contracts/mumbai.ts
@@ -1,11 +1,20 @@
 import { AbiItem } from "web3-utils";
 
-const contractAddress = "0xDC5Fd9220511A7211719dD8206Ec2D686054bCC0";
+const contractAddress = "0x009c403BdFaE357d82AAef2262a163287c30B739";
 const contractAbi: AbiItem[] = [
+  { inputs: [], stateMutability: "nonpayable", type: "constructor" },
   {
-    inputs: [],
-    stateMutability: "nonpayable",
-    type: "constructor",
+    anonymous: false,
+    inputs: [
+      {
+        indexed: false,
+        internalType: "string",
+        name: "message",
+        type: "string",
+      },
+    ],
+    name: "donateMessage",
+    type: "event",
   },
   {
     anonymous: false,
@@ -34,21 +43,16 @@ const contractAbi: AbiItem[] = [
     type: "event",
   },
   {
-    inputs: [
-      {
-        internalType: "uint256",
-        name: "a",
-        type: "uint256",
-      },
-    ],
+    inputs: [],
+    name: "_owner",
+    outputs: [{ internalType: "address", name: "", type: "address" }],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [{ internalType: "uint256", name: "a", type: "uint256" }],
     name: "arithmeticError",
-    outputs: [
-      {
-        internalType: "uint256",
-        name: "",
-        type: "uint256",
-      },
-    ],
+    outputs: [{ internalType: "uint256", name: "", type: "uint256" }],
     stateMutability: "pure",
     type: "function",
   },
@@ -60,27 +64,29 @@ const contractAbi: AbiItem[] = [
     type: "function",
   },
   {
-    inputs: [
-      {
-        internalType: "uint256",
-        name: "newValue",
-        type: "uint256",
-      },
-    ],
+    inputs: [{ internalType: "string", name: "message", type: "string" }],
+    name: "donate",
+    outputs: [],
+    stateMutability: "payable",
+    type: "function",
+  },
+  {
+    inputs: [{ internalType: "uint256", name: "newValue", type: "uint256" }],
     name: "setValue",
     outputs: [],
     stateMutability: "nonpayable",
     type: "function",
   },
   {
-    inputs: [
-      {
-        internalType: "uint256",
-        name: "newValue",
-        type: "uint256",
-      },
-    ],
+    inputs: [{ internalType: "uint256", name: "newValue", type: "uint256" }],
     name: "setValue2",
+    outputs: [],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+  {
+    inputs: [{ internalType: "address", name: "newOwner", type: "address" }],
+    name: "transferOwnership",
     outputs: [],
     stateMutability: "nonpayable",
     type: "function",
@@ -88,28 +94,24 @@ const contractAbi: AbiItem[] = [
   {
     inputs: [],
     name: "value",
-    outputs: [
-      {
-        internalType: "uint256",
-        name: "",
-        type: "uint256",
-      },
-    ],
+    outputs: [{ internalType: "uint256", name: "", type: "uint256" }],
     stateMutability: "view",
     type: "function",
   },
   {
     inputs: [],
     name: "value2",
-    outputs: [
-      {
-        internalType: "uint256",
-        name: "",
-        type: "uint256",
-      },
-    ],
+    outputs: [{ internalType: "uint256", name: "", type: "uint256" }],
     stateMutability: "view",
     type: "function",
   },
+  {
+    inputs: [],
+    name: "withdraw",
+    outputs: [],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
 ];
+
 export { contractAddress, contractAbi };
