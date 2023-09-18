6c552c8fd654f848a0973f9ac45d7681009b68c4
calvinchang <q20274982@gmail.com>
2023-09-18 14:15:52 +0800

    feat: add sendTx feat to UserOp template

--- src/components/EvmEditors/EvmUserOpEditor.tsx
+++ src/components/EvmEditors/EvmUserOpEditor.tsx
@@ -22,8 +22,22 @@ import { EthereumTypes } from "@blocto/sdk";
 import ParamEditor from "./ParamEditor";
 import * as UserOperationTemplate from "../../scripts/evm/UserOperation";
 import type { IUserOperationTemplate } from "../../scripts/evm/UserOperation";
+import EvmSendEditor from "./EvmSendEditor";
+import { AbiItem, numberToHex, isAddress, isHexStrict } from "web3-utils";
+import Web3EthAbi from "web3-eth-abi";
 
 const MenuGroups = [{ title: "Request", templates: UserOperationTemplate }];
+const ABI: AbiItem = {
+  inputs: [
+    { internalType: "address", name: "dest", type: "address" },
+    { internalType: "uint256", name: "value", type: "uint256" },
+    { internalType: "bytes", name: "func", type: "bytes" },
+  ],
+  name: "execute",
+  outputs: [],
+  stateMutability: "nonpayable",
+  type: "function",
+};
 
 const EvmUserOpEditor = ({
   setRequestObject,
@@ -34,10 +48,13 @@ const EvmUserOpEditor = ({
   >;
   account: string | null;
 }): ReactJSXElement => {
+  const [templateId, setTemplateId] = useState<string>("");
   const [callData, setCallData] = useState<string>("");
   const [otherParam, setOtherParam] = useState<string[][]>([]);
+
   const importTemplate = useCallback((template: IUserOperationTemplate) => {
-    setCallData(template.userOpObj.callData || "");
+    setTemplateId(template.id);
+    setCallData(template.userOpObj.callData);
     setOtherParam(
       Object.entries(template.userOpObj)
         .filter(([key]) => key !== "callData")
@@ -53,6 +70,27 @@ const EvmUserOpEditor = ({
       });
     }
   }, [account, callData, otherParam, setRequestObject]);
+
+  const setTransactionToCallData = useCallback(
+    ([params]) => {
+      const { to, value = "0x", data = "0x" } = params;
+
+      if (!isAddress(to) || !isHexStrict(data)) return;
+
+      const callData = Web3EthAbi.encodeFunctionCall(ABI, [
+        to,
+        numberToHex(value),
+        data,
+      ]);
+
+      setCallData(callData);
+      setRequestObject({
+        method: "eth_sendUserOperation",
+        params: [{ callData, ...Object.fromEntries(otherParam) }],
+      });
+    },
+    [otherParam, setRequestObject]
+  );
 
   return (
     <Flex flexDirection="column">
@@ -80,16 +118,25 @@ const EvmUserOpEditor = ({
         </MenuList>
       </Menu>
 
-      <Flex my="20px" alignItems="center">
-        <Box mx="10px">callData:</Box>
-        <Textarea
-          rows={2}
-          value={callData}
-          onChange={(e) => {
-            setCallData(e.target.value);
-          }}
-        />
-      </Flex>
+      {templateId === "sendTransaction" ? (
+        <Box my="20px">
+          <EvmSendEditor
+            setRequestObject={setTransactionToCallData}
+            account={account}
+          />
+        </Box>
+      ) : (
+        <Flex my="20px" alignItems="center">
+          <Box mx="10px">callData:</Box>
+          <Textarea
+            rows={2}
+            value={callData}
+            onChange={(e) => {
+              setCallData(e.target.value);
+            }}
+          />
+        </Flex>
+      )}
 
       <ParamEditor
         title="Other Param"
